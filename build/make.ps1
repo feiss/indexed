79175ef41c54b9c3ffc9f436977e5310e9931fdf
Diego <diego@feiss.be>
2015-10-05 01:10:46 +0200

    drawing example

--- build/make.ps1
+++ build/make.ps1
@@ -3,6 +3,10 @@
 echo "/** INDEXED" > indexed.min.js
 echo "*** indexed color mode for canvas, and powered by playground and twgl" >> indexed.min.js
 echo "*** indexed module copyright 2015 Diego F. Goberna"  >> indexed.min.js
+
+echo "*** playground by rezoner (http://github.com/rezoner/playground)" >> indexed.min.js
+echo "*** twgl by greggman (https://github.com/greggman/twgl.js)" >> indexed.min.js
+
 echo "*** see http://github.com/feiss/indexed"  >> indexed.min.js
 echo "*/" >> indexed.min.js
 
@@ -15,6 +19,11 @@ python -m jsmin ../src/indexed.render.js >> indexed.min.js
 echo "/** INDEXED" > indexed.js
 echo "*** indexed color mode for canvas, and powered by playground and twgl" >> indexed.js
 echo "*** indexed module copyright 2015 Diego F. Goberna"  >> indexed.js
+
+
+echo "*** playground by rezoner (http://github.com/rezoner/playground)" >> indexed.js
+echo "*** twgl by greggman (https://github.com/greggman/twgl.js)" >> indexed.js
+
 echo "*** see http://github.com/feiss/indexed"  >> indexed.js
 echo "*/" >> indexed.js
 
